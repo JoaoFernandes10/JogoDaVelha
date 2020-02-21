ceafca0e7a9d63397945231e64c75f8b7a025b15
Joao Fernandes <joao43013@gmail.com>
2020-02-21 12:14:23 -0300

    21/02/2020

---
+++ jogo-da-velha.py
@@ -0,0 +1,123 @@
+#!/data/user/0/org.qpython.qpy3/files/bin/qpython3-android5.sh
+from random import randint
+from os import system
+casa = ['',['','','',''],['','','',''],['','','','']]
+
+jogador = velha = ''
+
+def main():
+    global jogador, velha
+    tela()
+    if not escolherJogador():
+        return 0
+    else:
+        for i in range(1, 10):
+            if i % 2 != 0:
+                jog()
+                tela()
+                if jogador == vencedor():
+                    print('Parabens! voce venceu')
+                    break
+            else:
+                velh()
+                tela()
+                if velha == vencedor():
+                    print('Deu Velha!')
+                    break
+            if i == 9:
+                print('Deu Empate!')
+
+
+def jog():
+    global jogador
+    pos = input('Informe a Posicao Para {}: '.format(jogador))
+    while not jogadaValida(pos[0], int(pos[1])):
+        pos = input('Informe a Posicao Para {}: '.format(jogador))
+    escolherPosicao(pos[0], int(pos[1]), jogador)
+
+
+def velh():
+    global velha
+    letra = chr(randint(65, 67))
+    numero = randint(1, 3)
+    while not jogadaValida(letra, numero):
+        letra = chr(randint(65, 67))
+        numero = randint(1, 3)
+    escolherPosicao(letra, numero, velha)
+
+
+def jogadaValida(letra, numero):
+    l = letra.upper()
+    if l == 'A':
+        coluna = 1
+    elif l == 'B':
+        coluna = 2
+    elif l == 'C':
+        coluna = 3
+    else:
+        return False
+    if '' == casa[numero][coluna]:
+        return True
+    else:
+        return False
+
+def tela():
+    system('clear')
+    print('{:^20}'.format('Jogo da Velha'))
+    print('⁼'*32)
+    print('|{:^3}|{:^4}|{:^4}|{:^4}|'.format('||', 'A', 'B', 'C'))
+    print('='*20)
+    print('|{:^3}|{:^4}|{:^4}|{:^4}|'.format('1', casa[1][1], casa[1][2], casa[1][3]))
+    print('-'*20)
+    print('|{:^3}|{:^4}|{:^4}|{:^4}|'.format('2', casa[2][1], casa[2][2], casa[2][3]))
+    print('-'*20)
+    print('|{:^3}|{:^4}|{:^4}|{:^4}|'.format('3', casa[3][1], casa[3][2], casa[3][3]))
+    print('-'*20)
+
+def escolherJogador():
+    global jogador
+    global velha
+    jog = input('Escolha o que jogar X/0 : ')
+    jogador = jog.upper()
+    if jogador == 'X':
+        velha = '0'
+        return True
+    elif jogador == '0':
+        velha = 'X'
+        return True
+    else:
+        print('Caractere Inválido!\nVoce Perdeu por wo')
+        return False
+        
+
+def escolherPosicao(letra, numero, jogador):
+    l = letra.upper()
+    if l == 'A':
+        coluna = 1
+    elif l == 'B':
+        coluna = 2
+    else:
+        coluna = 3
+    
+
+    casa[numero][coluna] = jogador
+
+def vencedor():
+    venc = ''
+    global jogador, velha, casa
+    for i in range(1, 4):
+        if '' != casa[i][1] == casa[i][2] == casa[i][3]:
+            return casa[i][3]
+        if '' != casa[1][i] == casa[2][i] == casa[3][i]:
+            return casa[3][i]
+    if '' != casa[1][1] == casa[2][2] == casa[3][3]:
+        return casa[3][3]
+    if '' != casa[1][3] == casa[2][2] == casa[3][1]:
+        return casa[3][1]
+
+def continuar():
+    msg = input('comecar : 1 => sim / 0 => nao')
+    while msg == '1':
+        main ()
+        continuar ()
+continuar ()
---
+++ quebra-cabeca.py
@@ -0,0 +1,101 @@
+#!/data/user/0/org.qpython.qpy3/files/bin/qpython3--android5.sh
+from random import randint
+from os import system
+num = [[], [[]], [[]], [[]], [[]]]
+mist = [[], [[]], [[]], [[]], [[]]]
+
+
+
+def main():
+    criarSequencia()
+    misturarnum()
+    tela()
+    jogar()
+    if num == mist:
+        print('Parabéns! Você Venceu')
+    
+def jogar():
+    while num != mist:
+        pos1 = int(input('Informe a jogada: '))
+        if not bool(pos1):
+            print('Game Over')
+            break
+        else:
+            linha = int(str(pos1)[0])
+            coluna = int(str(pos1)[1])
+            if jogadaValida(linha, coluna):
+                pos2 = posicaoAtual()
+                trocarnum(pos1, pos2, mist)
+                tela()
+    
+def posicaoAtual():
+    for i in range(1, 5):# percorre linha
+        for j in range(1, 5):# percorre coluna
+            if mist[i][j] == "<|>":
+                pos = "{}{}".format(str(i), str(j))
+    return int(pos)
+
+def jogadaValida(linha, coluna, matriz=mist):
+    pos = posicaoAtual()
+    if is_vizinha(linha, coluna, pos):
+        return True
+    else:
+        return False
+
+def is_vizinha(linha, coluna, pos):
+    el = str(pos)
+    l = int(el[0])
+    c = int(el[1])
+    if linha > 0 < coluna and linha < 5 > coluna: 
+        if l == linha and c - 1 == coluna: # vizinha
+            return True
+        if l == linha and c + 1 == coluna: # vizinha
+            return True
+        if c == coluna and l - 1 == linha: # vizinha
+            return True
+        if c == coluna and l + 1 == linha: # vizinha
+            return True
+    return False
+
+def criarSequencia():
+    i = a = 1
+    while a <= 16:
+        num[i].append(a)
+        mist[i].append(a)
+        if a == 16:
+            mist[4].pop()
+            num[4].pop()
+            num[4].append('<|>')
+            mist[4].append('<|>')
+        if a % 4 == 0:
+            i += 1
+        
+        a += 1
+        
+
+def trocarnum(pos1, pos2, matriz=mist):
+    global num
+    el1 = matriz[pos1 // 10][pos1 % 10]
+    el2 = matriz[pos2 // 10][pos2 % 10]
+    matriz[pos1 // 10][pos1 % 10] = el2
+    matriz[pos2 // 10][pos2 % 10] = el1
+
+
+def misturarnum():
+    trocarnum(randint(11, 14), randint(21, 24))
+    trocarnum(randint(31, 34), randint(41, 43))
+    trocarnum(randint(11, 14), randint(21, 24))
+    trocarnum(randint(31, 34), randint(41, 43))
+
+def tela():
+    system('clear')
+    print("=" * 40)
+    print('|{:^38}|'.format('Digite 0 pra encerrar o jogo'))
+    print("=" * 40)
+    for i in range(1, 5):
+        print("=" * 40)
+        print("|{:^9}|{:^8}|{:^9}|{:^9}|".format(mist[i][1], mist[i][2], mist[i][3], mist[i][4]))
+    print("=" * 40)
+    print("=" * 40)
+
+main()
